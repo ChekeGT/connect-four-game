28e35aaedff6b3574bffe1e8467306bae96d46e2
Ezequiel <chekelosos@gmail.com>
2024-01-04 21:00:36 -0600

    Added the palette scheme of colors to the tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ export default {
       fontFamily: {
         spaceGrotesk: ['SpaceGrotesk', 'sans-serif'],
       },
+      colors: {
+        darkPurple: '#5C2DD5',
+        mainPurple: '#7945FF',
+        mainRed: '#FD6687',
+        mainYellow: '#FFCE67',
+      },
     },
   },
   plugins: [],
