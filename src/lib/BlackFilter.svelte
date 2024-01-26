24aadfa57d307e9665a2dc004f8476dbbc91934e
Ezequiel <chekelosos@gmail.com>
2024-01-25 22:18:10 -0600

    Fixed the height of the black filter on larger screens

--- src/lib/BlackFilter.svelte
+++ src/lib/BlackFilter.svelte
@@ -1,2 +1,5 @@
-<div class=" z-[79] w-full h-full absolute bg-[#00000099]">
+<script>
+    var height = window.innerWidth >= 500 ? window.innerHeight + 200 : window.innerHeight
+</script>
+<div style={`height:${height}px`} class=" z-[79] w-full absolute bg-[#00000099]">
 </div>
--- src/lib/GameViewMenu.svelte
+++ src/lib/GameViewMenu.svelte
@@ -1,14 +1,26 @@
 <script>
     let { minHeight } = $props()
     import { game } from "../store.svelte";
+    function getHeight(minHeight){
+        if (window.innerWidth < 400) {
+            return minHeight - 20
+        }else if (window.innerWidth < 1024) {
+            return minHeight - 70
+        }
+        else{
+            return minHeight - 200
+        }
+    }
 </script>
 
-<div style={`min-height: ${minHeight}px;`} class=" font-spaceGrotesk absolute w-11/12  left-[50%] translate-x-[-50%] z-[80] card bg-mainPurple py-4 ">
-    <div class=" flex flex-col w-9/12 mx-auto gap-5">
+<div style={`height: ${getHeight(minHeight)}px`} class=" my-[20px] max-w-[480px] lg:my-[90px] font-spaceGrotesk absolute w-11/12  left-[50%] translate-x-[-50%] top-0 z-[80] card bg-mainPurple py-4 ">
+    <div class=" flex flex-col w-9/12 mx-auto gap-3 h-full md:gap-5">
         <p class=" font-2xl text-center text-white text-3xl font-bold">PAUSE</p>
-        <button on:click={() => { game.gameState = 'playing'}} class=" card bg-white p-3">CONTINUE</button>
-        <button on:click={game.resetBoard} class=" card bg-white p-3">RESTART</button>
-        <button on:click={() => {game.gameState = 'initialMenu'}} class=" card bg-mainRed text-white p-3">QUIT GAME</button>
+        <div class=" flex justify-between flex-col gap-5">    
+            <button on:click={() => { game.gameState = 'playing'}} class=" card bg-white p-3">CONTINUE</button>
+            <button on:click={game.resetBoard} class=" card bg-white p-3">RESTART</button>
+            <button on:click={() => {game.gameState = 'initialMenu'}} class=" card bg-mainRed text-white p-3">QUIT GAME</button>
+        </div>
     </div>
 </div>
 
